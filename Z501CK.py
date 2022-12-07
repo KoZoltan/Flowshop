da1178c7b474e4b3336b8825d07954664b7e5805
K.Zoltan <91243037+KoZoltan@users.noreply.github.com>
2022-12-07 23:35:36 +0100

    gantt diagramos kibővítés
    a diagrammot egy html file-ba generálja

--- Z501CK.py
+++ Z501CK.py
@@ -1,5 +1,10 @@
 import random
 import math
+##import plotly.figure_factory as ff
+import plotly.express as ff
+import pandas as pd
+import plotly
+
 
 def generalas(gen_genAlsoHatar, gen_genFelsoHatar, munkaSzam_szam, gepekSzam_szam):
     alapLista=[]
@@ -76,6 +81,38 @@ def cMax(rendlista,munkaSzamSzam,gepekSzamSzam):
     cmax=lista[munkaSzamSzam-1][gepekSzamSzam-1]
     return cmax
 
+def cMaxGant(rendlista,munkaSzamSzam,gepekSzamSzam):
+    lista = []
+    
+    for i in range(munkaSzamSzam):
+        temp = []
+        for j in range(gepekSzamSzam):
+            if i == 0:
+                if j == 0:
+                    temp.append(rendlista[i][j])
+                else:
+                    ertek = rendlista[i][j]+temp[j-1]
+                    temp.append(ertek)
+                    
+                if j == gepekSzamSzam-1:
+                    lista.append(temp)
+            else:
+                if j == 0:
+                    ertek = lista[i-1][j]+rendlista[i][j]
+                    temp.append(ertek)
+                else:
+                    if temp[j-1]-lista[i-1][j] >= 0:
+                        ertek = rendlista[i][j]+temp[j-1]
+                        temp.append(ertek)
+                    else:
+                        var=lista[i-1][j]-temp[j-1]
+                        ertek = rendlista[i][j]+temp[j-1]+var
+                        temp.append(ertek)
+                if j == gepekSzamSzam-1:
+                    lista.append(temp)
+    return lista
+
+
 def valamilista(vegleges,sorrend,cmax):
     vegleges.append([sorrend,cmax])
     return vegleges
@@ -86,17 +123,13 @@ def mutacio(sorrend, munkaSzamSzam):
     while melyik == hova:
         melyik = random.randint(0,munkaSzamSzam-1)
         hova = random.randint(0,munkaSzamSzam-1)
-##    print(melyik, hova)
     lista = []
     for i in range(munkaSzamSzam):
         lista.append(sorrend[i])
-##    print("csere előt")
-##    print(lista)
+
     tmp = lista[hova]
     lista[hova]=lista[melyik]
     lista[melyik] = tmp
-##    print("csere után")
-##    print(lista)
     return lista
    
 def optimum(veglegeslista):
@@ -105,9 +138,12 @@ def optimum(veglegeslista):
         elem.append(veglegeslista[i][1])
     minimum = min(elem)
 
+    lista = []
     for i in range(len(veglegeslista)):
         if veglegeslista[i][1] == minimum:
-            print("az optimum:", veglegeslista[i])
+            lista.append([veglegeslista[i]])
+    return lista[0]
+    
 
 def feladat(alaplista,munkaSzam,leallasiFeltetl,gepekSzam):
     vegleges=[]
@@ -127,34 +163,88 @@ def feladat(alaplista,munkaSzam,leallasiFeltetl,gepekSzam):
             vegleges = valamilista(vegleges,temp,cmax)
         else:
             temp = mutacio(temp,munkaSzam)
-            
-    
-    
-    
-    return vegleges
+    veg = optimum(vegleges)
+    print("optimális sorrend:",veg)
+    ertekek = []
+    jobok = []
+    for i in range(munkaSzam):
+        for j in range(munkaSzam):
+            if veg[0][0][i] == alaplista[j][0][0]:
+                ertekek.append(alaplista[j][1])
+                jobok.append(veg[0][0][i])
+    ertekek = cMaxGant(ertekek,munkaSzam,gepekSzam)
 
+    ganttLista = []
+    for i in range(munkaSzam):
+        for j in range(gepekSzam):
+            tempLista = []
+            if i ==0:
+                if j==0:
+                    start=0
+                    bef=ertekek[i][j]
+                    tempLista.append(jobok[i])
+                    tempLista.append(start)
+                    tempLista.append(bef)
+                    tempLista.append("Green")
+                    ganttLista.append(tempLista)
+                else:
+                    start = ertekek[i][j-1]
+                    bef = ertekek[i][j]
+                    tempLista.append(jobok[i])
+                    tempLista.append(start)
+                    tempLista.append(bef)
+                    tempLista.append("Green")
+                    ganttLista.append(tempLista)
+            else:
+                if j==0:
+                    start = ertekek[i-1][j]
+                    bef = ertekek[i][j]
+                    tempLista.append(jobok[i])
+                    tempLista.append(start)
+                    tempLista.append(bef)
+                    tempLista.append("Green")
+                    ganttLista.append(tempLista)
+                else:
+                    start = ertekek[i][j-1]
+                    bef = ertekek[i][j]
+                    tempLista.append(jobok[i])
+                    tempLista.append(start)
+                    tempLista.append(bef)
+                    tempLista.append("Green")
+                    ganttLista.append(tempLista)
+                    
+      
+
+##    print(ganttLista)
+    df = pd.DataFrame(ganttLista,columns=['name','start','end', 'color'])
+    print(df)
+
+    fig = ff.timeline(df, x_start=df['start'], x_end=df['end'],y=df['name'], color=df['color'], title='Flow-shop')
+    fig.update_yaxes(autorange="reversed")
+    fig.show()
+    plotly.offline.plot(fig, filename='Task_Flowshop_Gantt.html')
+    
 
 
 def main():
     genAlsoHatar = 1
     genFelsoHatar = 10
-    munkaSzam = 50
-    gepekSzam = 10
-    leallasiFeltetl = 1000 ## ezt fixen el tudja kezelni
-
+    munkaSzam = 10
+    gepekSzam = 4
+    
+    leallasiFeltetl = int(input("Adja meg a leállási feltételt: ")) 
 
     alaplista =(generalas(genAlsoHatar,genFelsoHatar,munkaSzam,gepekSzam))
-##    print(alaplista)
     print("alaplista")
     for i in range(len(alaplista)):
         print(alaplista[i])
     print("feladatmegoldas")
-    veg = feladat(alaplista,munkaSzam,leallasiFeltetl,gepekSzam)
-    optimum(veg)
+    feladat(alaplista,munkaSzam,leallasiFeltetl,gepekSzam)
+    
+
     
 if __name__ == "__main__":
     main()
-
 
 
 
